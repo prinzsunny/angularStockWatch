8b06ceb13f88eafcb365f66fa335418702b517eb
prinzsunny <prinzsunny07@gmail.com>
2014-11-09 19:23:02 -0800

    Initial push

---
+++ StockWatcher/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StockWatcher::Application.config.secret_token = '23da553fe9dc6a49139aa21151ef09633bd0b1ec22a5a5d7198b7296831cc392e176a0fe510936c56bc2f7c907cd9f6cd72a72b639fe02b3500d963d018fa25d'
